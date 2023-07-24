31013250ae1179ccad54b05c64d66be060ae49a5
Sanino Fabio <fabiosanino@gmail.com>
2023-07-24 15:52:11 +0200

    begin personaGiuridica class

---
+++ js/PersonaGiuridica.js
@@ -0,0 +1,26 @@
+class PersonaGiuridica {
+
+
+    constructor(){
+
+        this.struttura = {
+
+            cognome: "string",
+            nome: "string",
+            cap: "number",
+
+            metodo: [""],
+            valore: "string",
+
+            numeroParenti: "number",
+            coniuge: "boolean",
+            padre: "boolean",
+            madre: "boolean",
+            figli: "number",
+
+            attivita: [""],
+            immobili: [""],
+            animali: [""]
+        }
+    }
+}
